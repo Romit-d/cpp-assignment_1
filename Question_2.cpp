2f8d781ad7a3da9725e12d7f364a5bfb748c75c2
Romit-d <160019577+Romit-d@users.noreply.github.com>
2024-02-14 17:00:53 +0530

    Solved assignment files are added.

---
+++ Question_2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 5;
+
+    cout<<"By adding two positive intigers which is < 6 is -  "<<a+b<<endl;
+    return 0;
+}
---
+++ Question_5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius;
+    float pi = 3.14;
+    cout<<"Enter the radius -";
+    cin>>radius;
+    cout<<"The circumference of a circle is "<<2*pi*radius<<endl;
+    return 0;
+}
